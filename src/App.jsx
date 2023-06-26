0feca37146faa64eefc897f168c53f4696a86391
magywairigu <wairigumaina2016@gmail.com>
2023-06-26 12:38:20 +0300

    navbar complete, missing dependencies added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { BrowserRouter } from 'react-router-dom'; //for routing
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, StarsCanvas, Tech, Works } from './components';
 
 const App = () => {
 
   return (
-    <>
-      <div>Maggy's portfolio </div>
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
         
-    </>
+    </BrowserRouter>
   )
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../style';
+import {ComputersCanvas} from './canvas'
 
 const Hero = () => {
   return (
