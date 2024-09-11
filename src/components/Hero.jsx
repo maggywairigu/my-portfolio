e3357e95a4855b9ed4f5b1e5b20a6a0a88ee3f68
maggywairigu <68754799+maggywairigu@users.noreply.github.com>
2024-09-11 23:43:11 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#d6328e]' />
-          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+          <div className='w-1 sm:h-80 h-40 pink-gradient'/>
         </div>
         <div>
             <h1 className={`${styles.heroHeadText} text-white`}>Hi! I'm <span className="text-[#d6328e]">Margaret</span></h1>
