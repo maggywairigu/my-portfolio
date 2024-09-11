63fbc07f25d8f469e0371995f3b923fd7d33ce58
maggywairigu <68754799+maggywairigu@users.noreply.github.com>
2024-09-11 23:41:40 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,13 +8,13 @@ const Hero = () => {
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
+          <div className='w-5 h-5 rounded-full bg-[#d6328e]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient'/>
         </div>
         <div>
-            <h1 className={`${styles.heroHeadText} text-white`}>Hi! I'm <span className="text-[#915eff]">Margaret</span></h1>
+            <h1 className={`${styles.heroHeadText} text-white`}>Hi! I'm <span className="text-[#d6328e]">Margaret</span></h1>
             <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              I develop dynamic and interactive applications <br className='sm:block hidden'/> using HTML, CSS, JavaScript, and React.
+              Welcome to my portfolio. <br className='sm:block hidden'/>I am a full-stack software developer.
             </p>
           </div>
       </div>
